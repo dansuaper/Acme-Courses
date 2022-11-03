7fd39dbf27e325aa7e937c605d81222fcf439c66
dansuaper <dansuaper@alum.us.es>
2022-11-03 17:13:09 +0100

    Task 020: Done

---
+++ src/main/java/acme/entities/courses/Course.java
@@ -0,0 +1,62 @@
+package acme.entities.courses;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.Transient;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.datatypes.Money;
+import acme.framework.entities.AbstractEntity;
+import acme.roles.Teacher;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Course extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	protected boolean published;
+	
+	@Column(unique = true)
+	@Pattern(regexp = "^[A-Z]{3}-[0-9]{3}(:[A-Z]{1,10})?$") 
+	protected String ticker;
+
+	@NotBlank
+	@Length(min=1, max=75)
+	protected String caption;
+	
+	@NotBlank
+	@Length(min=1, max=255)
+	protected String abstractText;
+	
+	@URL
+	protected String link;
+	
+	@Valid
+	@Transient
+	protected Money retailPrice;
+	
+	// Derived attributes -----------------------------------------------------
+	
+	// Relationships ----------------------------------------------------------
+	
+	@NotNull
+	@Valid
+	@ManyToOne(optional=false)
+	protected Teacher teacher;
+	
+}
