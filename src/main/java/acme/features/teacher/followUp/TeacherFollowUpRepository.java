ed3237c0c833abc2e5df229d5f444fd37a93f84b
dansuaper <dansuaper@alum.us.es>
2022-11-12 14:12:29 +0100

    Task 042: Done

---
+++ src/main/java/acme/features/teacher/followUp/TeacherFollowUpRepository.java
@@ -0,0 +1,22 @@
+package acme.features.teacher.followUp;
+import java.util.Collection;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import acme.entities.followUps.FollowUp;
+import acme.framework.repositories.AbstractRepository;
+import acme.roles.Teacher;
+ 
+@Repository 
+public interface TeacherFollowUpRepository extends AbstractRepository { 
+	 
+	@Query("Select f from FollowUp f where f.helpRequest.id = :id") 
+	Collection<FollowUp> findFollowUpsByHelpRequestId(int id); 
+	 
+	@Query("Select t from Teacher t where t.userAccount.id = :id")
+	Teacher findTeacherByUserAccountId(int id); 
+	
+	@Query("Select f from FollowUp f where f.id = :id") 
+	FollowUp findFollowUpById(int id); 
+} 
---
+++ src/main/webapp/WEB-INF/views/teacher/follow-up/list.jsp
@@ -0,0 +1,10 @@
+<%@page language="java"%> 
+ 
+<%@taglib prefix="jstl" uri="http://java.sun.com/jsp/jstl/core"%> 
+<%@taglib prefix="acme" uri="urn:jsptagdir:/WEB-INF/tags"%> 
+ 
+<acme:list> 
+	<acme:list-column code="teacher.followUp.list.label.sequenceNumber" path="sequenceNumber"/> 
+	<acme:list-column code="teacher.followUp.list.label.instantiationMoment" path="instantiationMoment"/>
+	<acme:list-column code="teacher.followUp.list.label.info" path="info"/>
+</acme:list>                                                                                               
